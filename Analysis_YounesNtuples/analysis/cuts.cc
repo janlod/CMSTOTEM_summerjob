d330660653c9c5c45e700b61ef05604430688a7f
Jan Loder <jan.loder@helsinki.fi>
2025-07-01 16:59:56 +0200

    Started reimplementing cuts, but now all at the same time

--- Analysis_YounesNtuples/analysis/cuts.cc
+++ Analysis_YounesNtuples/analysis/cuts.cc
@@ -132,3 +132,83 @@ void cut_dzdzerr(TTree* tree, std::string filebasename){
     outfile->Close();
     delete outfile;
 }
+
+bool isin_3sigma(Float_t val, Float_t mean, Float_t sigma){
+   return (val <= mean + 3*sigma && val >= mean - 3*sigma);
+}
+
+void gaussian_3sigma_cut(TTree* tree, std::string filebasename){
+    Float_t zPV;
+    Int_t ntrk;
+    Float_t trk_dz[1000];
+    Float_t trk_dzerr[1000];
+    Float_t trk_dxy[1000];
+    Float_t trk_dxyerr[1000];
+    Float_t trk_eta[1000];
+    Float_t trk_phi[100];
+
+    tree->SetBranchAddress("zPV", &zPV);
+    tree->SetBranchAddress("ntrk", &ntrk);
+    tree->SetBranchAddress("trk_dz", trk_dz);
+    tree->SetBranchAddress("trk_dzerr", trk_dzerr);
+    tree->SetBranchAddress("trk_dxy", trk_dxy);
+    tree->SetBranchAddress("trk_dxyerr", trk_dxyerr);
+    tree->SetBranchAddress("trk_eta", trk_eta);
+    tree->SetBranchAddress("trk_phi", trk_phi);
+
+    TF1* fit_zPV = new TF1("fit_zPV", "gaus", -5, 5);
+    TF1* fit_dz = new TF1("fit_dz", "gaus", -1, 1);
+    TF1* fit_dxy_phi = new TF1("fit_dxy_phi", "gaus", -0.1, 0.1);
+    TF1* fit_dz_eta = new TF1("fit_dz_eta", "gaus", -0.1, 0.1);
+
+    TH1F* hist_zPV = new TH1F("zPV", "zPV", 400, -15, 15);
+    TH1F* hist_dzBydzerr = new TH1F("dz/dzerr", "dz/dzerr", 400, -3, 3);
+    TH1F* hist_dxyBydxyerr = new TH1F("dxy/dxyerr", "dxy/dxyerr", 400, -3, 3);
+    TH2F* hist_dxy_phi = new TH2F("dxy vs. phi", "dxy vs. phi", 400, -0.3, 0.3, 400, -3.14, 3.14);
+    TH2F* hist_dz_eta = new TH2F("dz vs. eta", "dz vs. eta", 400, -0.5, 0.5, 400, -3.0, 3.0);
+
+    Long64_t nentries = tree->GetEntries();
+    double sigma_zPV, mean_zPV;
+    double sigma_dzBydzerr, mean_dzBydzerr;
+    double sigma_dxyBydxyerr, mean_dxyBydxyerr;
+    for(int i=0; i<nentries; i++){
+        tree->GetEntry(i);
+        if(ntrk == 4){
+            hist_zPV->Fill(zPV);
+            for(int itrk=0; itrk<ntrk; itrk++){
+                hist_dzBydzerr->Fill(trk_dz[itrk]/trk_dzerr[itrk]);
+                hist_dxyBydxyerr->Fill(trk_dxy[itrk]/trk_dxyerr[itrk]);
+                hist_dxy_phi->Fill(trk_dxy[itrk], trk_phi[itrk]);
+                hist_dz_eta->Fill(trk_dz[itrk], trk_eta[itrk]);
+            }
+        }
+    }
+    hist_zPV->Fit(fit_zPV, "R");
+    mean_zPV = fit_zPV->GetParameter(1);
+    sigma_zPV = fit_zPV->GetParameter(2);
+
+    hist_dzBydzerr->Fit(fit_dz, "R");
+    mean_dzBydzerr = fit_dz->GetParameter(1);
+    sigma_dzBydzerr = fit_dz->GetParameter(2);
+
+    hist_dxyBydxyerr->Fit(fit_dxy, "R");
+    mean_dxyBydxyerr = fit_dxy->GetParameter(1);
+    sigma_dxyBydxyerr = fit_dxy->GetParameter(2);
+
+    //Figure out what to do about 2d plots and fits and then loop through tree and do something along the lines:
+    /**TFile* inputFile = TFile::Open("input.root");
+TTree* tree = (TTree*)inputFile->Get("tree");
+
+TFile* outputFile = new TFile("output.root", "RECREATE");
+
+// Apply cuts like: var1 > 0 && var2 < 10
+TTree* cutTree = tree->CopyTree("var1 > 0 && var2 < 10");
+
+cutTree->Write();
+outputFile->Close();
+inputFile->Close();
+
+
+     */
+
+}
